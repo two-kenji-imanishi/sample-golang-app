7b6358a5ba8698a5c93f94542c16382d83923b8b
two-kenji-imanishi <kenji-imanishi@two2.jp>
2022-10-03 12:20:15 +0900

    ハンドラを追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Welcome to World!\n")
+}
+
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello GAE World 2022!\n")
 }
